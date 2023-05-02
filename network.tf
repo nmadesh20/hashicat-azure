fee3b4a4dd75cae934bc3c8542e66d35e65ffe20
Madesh Nagaraj <you@example.com>
2023-05-02 19:12:18 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/Madesh-Training/network/azurerm"
-  version = "5.2.0"
+  version = "3.5.0"
   # insert required variables here
   resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
